1f87bdf239224208a884aa5d359a2ee5aee338e9
Carlos Collazos <ccollaz@bancodebogota.com.co>
2026-05-04 23:18:56 -0500

    feat: add Models

--- PaComer/Models/Product.swift
+++ PaComer/Models/Product.swift
@@ -5,3 +5,14 @@
 //  Created by Collazos Zambrano, Carlos Daniel on 4/05/26.
 //
 
+import Foundation
+
+struct Product: Identifiable, Codable, Hashable {
+    var id: String = UUID().uuidString
+    var name: String = ""
+    var description: String = ""
+    var price: Double = 0.0
+    var category: String = ""
+    var imageUrl: String = ""
+    var extras: [Extra] = []
+}
